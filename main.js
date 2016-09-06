de7cbdac61854dc4f77b2bd57708b1b5fb0419ae
James Skemp <strivinglife@gmail.com>
2016-09-05 19:05:26 -0500

    Add functionality to actual implement cursor purchasing.

--- main.js
+++ main.js
@@ -11,8 +11,14 @@ function buyCursor() {
 	// Cursors cost = 10 * 1.1^(cursors)
 	var cursorCost = Math.floor(10 * Math.pow(1.1, cursors));
 
-	cursors++;
+	if (cookies >= cursorCost) {
+		cursors++;
+		cookies -= cursorCost;
 
-	document.getElementById('cursors').innerHTML = cursors;
-	document.getElementById('cursorCost').innerHTML = cursorCost;
+		document.getElementById('cursors').innerHTML = cursors;
+		document.getElementById('cookies').innerHTML = cookies;
+	}
+
+	var nextCursorCost = Math.floor(10 * Math.pow(1.1, cursors));
+	document.getElementById('cursorCost').innerHTML = nextCursorCost;
 }
