bdffed0cb4adbd46cc6cd6e2896696cff6bbf8e6
James Skemp <strivinglife@gmail.com>
2016-09-05 19:01:15 -0500

    Add very basic cursor functionality.
    Displays count and cost only.

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 ﻿var cookies = 0;
+var cursors = 0;
 
 function cookieClick(number) {
 	cookies += number;
 
 	document.getElementById('cookies').innerHTML = cookies;
+}
+
+function buyCursor() {
+	// Cursors cost = 10 * 1.1^(cursors)
+	var cursorCost = Math.floor(10 * Math.pow(1.1, cursors));
+
+	cursors++;
+
+	document.getElementById('cursors').innerHTML = cursors;
+	document.getElementById('cursorCost').innerHTML = cursorCost;
 }
